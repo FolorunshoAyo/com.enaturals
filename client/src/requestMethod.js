dda150fae1848b12b07252bdffc6eac735099283
Shodiya Folorunsho <folushoayomide11@gmail.com>
2022-07-20 10:35:04 +0100

    Bug fixes

--- client/src/requestMethod.js
+++ client/src/requestMethod.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://enaturalsskincare.co/api";
+const BASE_URL = "http://enaturalsskincare.co";
 
 export const publicRequest = axios.create({
     baseURL: BASE_URL
